af86f10c5d8e539771de89aa1a6bdea289e322d5
2553589948 <2553589948@qq.com>
2019-12-04 18:38:53 +0800

    响应式布局

--- vue/vue-loveread/src/components/page/Story.vue
+++ vue/vue-loveread/src/components/page/Story.vue
@@ -9,7 +9,223 @@
         <input type="text" class="search-input" placeholder="请输入你要搜索的关键词" />
       </div>
     </div>
-    <div class="novels-container"></div>
+    <div class="novels-container">
+      <!-- 排行榜 -->
+      <div class="topCategory">
+        <div class="ranking-block">
+          <div class="ranking-block-header">
+            <div class="ranking-block-header-link">
+              <img src="https://rescdn.qqmail.com/weread/cover/ranklist.all.chart_title.NB681FwUw1.png" alt="">
+            </div>
+          </div>
+          <div class="ranking-block-body">
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="ranking-block-footer">
+            <div class="ranking-block-footer-link">
+              <span class="ranking-block-footer-link-text">查看全部</span>
+              <i class="icon">&#xe606;</i>
+            </div>
+          </div>
+        </div>
+        <div class="ranking-block">
+          <div class="ranking-block-header">
+            <div class="ranking-block-header-link">
+              <img src="https://rescdn.qqmail.com/weread/cover/ranklist.all.chart_title.NB681FwUw1.png" alt="">
+            </div>
+          </div>
+          <div class="ranking-block-body">
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="left">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体（全集）
+                    </p>
+                    <div class="ranking-block-book-author">刘慈欣</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right">
+              <div class="ranking-block-book">
+                <div class="ranking-block-book-container">
+                  <div class="ranking-block-book-cover">
+                    <img src="" alt="">
+                  </div>
+                  <div class="ranking-block-book-info">
+                    <p class="ranking-block-book-title">
+                      <span class="ranking-block-book-index">1</span>三体(全集)
+                    </p>
+                    <div class="ranking-block-book-author"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="ranking-block-footer">
+            <div class="ranking-block-footer-link">
+              <span class="ranking-block-footer-link-text">查看全部</span>
+              <i class="icon">&#xe606;</i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 全部分类 -->
+      <div class="allCategory"></div>
+    </div>
   </div>
 </template>
 
@@ -50,9 +266,12 @@ export default {
   font-family PingFang SC, sans-serif, STHeitiSC-Light, Helvetica-Light, arial
   background rgba(8, 5, 58, 0.9)
   color #fff
+  padding-bottom 57px
   .novels-header
     width 100%
     padding-bottom 80px
+    @media screen and (max-width: 460px)
+      padding-bottom 40px
     .search-top
       text-align center
       width 100%
@@ -99,4 +318,125 @@ export default {
   .novels-container
     max-width 1332px
     margin 0 auto
+    .topCategory
+      display flex
+      flex-wrap wrap
+      justify-content space-between
+      .ranking-block
+        width 48%
+        padding 0 40px
+        margin-bottom 22px
+        box-sizing border-box
+        @media screen and (max-width: 1020px)
+          width 100%
+          margin-bottom 14px
+        @media screen and (max-width: 960px)
+          padding 0 30px
+        &-header
+          padding 20px 0
+          border solid hsla(0, 0%, 100%, .05)
+          border-width 0 0 1px
+          &-link
+            display inline-block
+            img
+              display inline-block
+              width 176px
+              vertical-align bottom
+        &-body
+          .left
+            width 48%
+            float left
+            @media screen and (max-width: 560px)
+              width 100%
+              float none
+          .right
+            width 48%
+            float right
+            @media screen and (max-width: 560px)
+              width 100%
+              float none
+            @media screen and (max-width: 460px)
+              display none
+          .left, .right
+            .ranking-block-book
+              position relative
+              border solid hsla(0, 0%, 100%, .05)
+              border-width 0 0 1px
+              @media screen and (max-width: 460px)
+                position relative
+                border none
+              &-container
+                padding 16px 0 16px 16px
+                margin-left -16px
+                overflow hidden
+                .ranking-block-book-cover
+                  float left
+                  display block
+                  width 64px
+                  height 92px
+                  box-shadow 0 2px 16px rgba(0,0,0,.08)
+                  background #d8d8d8
+                  position relative
+                  img
+                    vertical-align top
+                    width 100%
+                    height 100%
+                    background-color #d8d8d8
+                .ranking-block-book-info
+                  padding 0 10px 0 82px
+                  margin-top 20px
+                  .ranking-block-book-title
+                    font-size 15px
+                    font-weight 500
+                    color #eef0f4
+                    overflow hidden
+                    white-space nowrap
+                    text-overflow ellipsis
+                    word-break break-all
+                    word-wrap normal
+                    .ranking-block-book-index
+                      width 18px
+                      display inline-block
+                      font-family DIN-Medium,PingFang SC,-apple-system,SF UI Text,Lucida Grande,STheiti,Microsoft YaHei,sans-serif
+                      font-size 16px
+                  .ranking-block-book-author
+                    position relative
+                    display inline-block
+                    margin-top 6px
+                    margin-left 18px
+                    font-size 12px
+                    z-index 2
+                    color #8a8c90
+                    overflow hidden
+                    white-space nowrap
+                    text-overflow ellipsis
+                    word-break break-all
+                    word-wrap normal
+        &-body::after
+          content ""
+          display block
+          clear both
+        &-footer
+          padding 20px 0
+          &-link
+            font-size 15px
+            color #b2b4b8
+            @media screen and (max-width: 460px)
+              width 100%
+              height 44px
+              line-height 44px
+              display inline-block
+              text-align center
+              color #1b88ee
+              border-radius 10px
+              border 1px solid hsla(0,0%,100%,.2)
+            &-text
+              vertical-align middle
+              margin-right 5px
+            .icon
+              font-family "iconfont"
+              font-style normal
+              vertical-align middle
+              @media screen and (max-width: 460px)
+                display none
 </style>
