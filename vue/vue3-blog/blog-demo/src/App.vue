36b067b4ddc043ebfd3c4b86042d8cc920079868
2553589948 <2553589948@qq.com>
2020-01-07 17:03:22 +0800

    建立自己的blog

--- vue/vue3-blog/blog-demo/src/App.vue
+++ vue/vue3-blog/blog-demo/src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
     <h1>cliver blog</h1>
-    <BlogPost></BlogPost>
-    <router-view/>
+    <button @click="changeColor">show/hide highlighted</button>
+    <BlogPost v-for="blogPost in visibileBlogPosts" :key="blogPost.title" :post="blogPost"></BlogPost>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script lang="ts">
-import BlogPost from "./components/BlogPost.vue";
-import { Component, Vue } from 'vue-property-decorator';
+import BlogPost, {Post} from "./components/BlogPost.vue";
+import { Component,Vue } from 'vue-property-decorator';
 @Component({
   components: {
     BlogPost
@@ -16,7 +17,37 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 
 export default class App extends Vue {
+  private blogPosts: Post[] = [
+    {
+      title: 'my name is kyle',
+      body: 'welcome to my blog',
+      author: 'kyle',
+      datePosted: new Date(2020, 0, 7) // 不取具体的时分秒
+    },
+    {
+      title: 'my name is kyle111',
+      body: 'welcome to my blog',
+      author: 'kyle',
+      datePosted: new Date(2020, 0, 8), // 不取具体的时分秒
+      highlighted: true
+    },
+    {
+      title: 'my name is kyle222',
+      body: 'welcome to my blog',
+      author: 'kyle',
+      datePosted: new Date(2020, 0, 9) // 不取具体的时分秒
+    }
+  ]
 
+  public showhighlighted: boolean = true
+
+  get visibileBlogPosts () {
+    return this.blogPosts.filter((post: Post) => post.highlighted === undefined || post.highlighted === this.showhighlighted)
+  }
+
+  public changeColor () {
+    this.showhighlighted = !this.showhighlighted
+  }
 }
 </script>
 
--- vue/vue3-blog/blog-demo/src/components/BlogPost.vue
+++ vue/vue3-blog/blog-demo/src/components/BlogPost.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="blogpost">
-    <h2>test</h2>
+  <div class="blogpost" :class="{highlighted: post.highlighted}">
+    <h2>{{post.title}}</h2>
+    <p v-if="post.highlighted">this post is highlighted</p>
+    <p>{{post.body}}</p>
+    <p class="meta">Written by {{post.author}} on {{date}}</p>
   </div>
 </template>
 
-<script>
-export default {
-
+<script lang='ts'>
+import { Vue, Prop, Component } from 'vue-property-decorator';
+export interface Post {
+  title: string;
+  body: string;
+  author: string;
+  datePosted: Date;
+  highlighted?: boolean;
+}
+@Component
+export default class BlogPost extends Vue {
+  @Prop() post!: Post
+  get date() {
+    return `${this.post.datePosted.getDate()}/${this.post.datePosted.getMonth()+1}/${this.post.datePosted.getFullYear()}`
+  }
 }
 </script>
 
 <style lang="stylus">
 .blogpost
+  width 400px
+  margin 0 auto
   h2
     text-decoration underline
+  .meta
+    font-style italic
+.highlighted
+  border 1px solid #f4d942
+  background #fff3b2
 </style>
