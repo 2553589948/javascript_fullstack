7bc012fab56ba9999691d673f4d6c1c53d81d66c
2553589948 <2553589948@qq.com>
2020-02-15 16:37:47 +0800

    react生命周期与操作数据源

--- react/learn/src/index.js
+++ react/learn/src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//入口
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// class App extends React.Component {
+//   render () {
+//     //   props
+//     return <div>hello {this.props.name}, I am {9+9} years old</div>
+//   }
+// }
+// state  数据源
+// 生命周期
+class Counter extends React.Component {
+  constructor (props) {
+    super(props)
+    //初始化构造器时设置内部状态 num值为1
+    this.state = {
+      num: 1
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+  
+//   生命周期方法，组件渲染完成
+  componentDidMount() {
+    console.log('componentDidMount 函数触发')
+  }
+//   生命周期方法 避免组件重复和无意义的渲染 组件更新时使用
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps,nextState)
+    if (nextState.num%2) {
+      return true
+    }
+    return false
+  }
+  handleClick () {
+    console.log(123)
+    this.setState({
+      num: this.state.num + 1
+    })
+  }
+  render () {
+    return ( 
+      <div>
+        <p>{this.state.num}</p>
+        <button onClick={this.handleClick}>click</button>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Counter/>,document.querySelector('#root'))
